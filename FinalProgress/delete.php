65701790fb0c3bb41fc5789ccd8fbae2a070e350
laurenmacpherson <60455659+laurenmacpherson@users.noreply.github.com>
2020-12-08 19:07:29 -0800

    Update delete.php

--- FinalProgress/delete.php
+++ FinalProgress/delete.php
@@ -6,7 +6,7 @@
       {
             $connect = new PDO('mysql:host=localhost;dbname=time_crunch_planner', 'root', '');
 
-             $id = $_POST['id'];
+             $id = $_POST['id']; //this is the event ID number, and when we click on the event we receive the ID number 
 
              $query = "
              DELETE from events WHERE eventNum='$id'
